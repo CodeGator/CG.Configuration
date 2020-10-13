d68b8ed610ec6a7b85100494f07cae0cfd32ca4b
martycook <martinc@valornet.com>
2020-10-13 14:08:38 -0500

    initial commit

---
+++ src/CG.Configuration/AppConfig/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,47 @@
+﻿using CG.Configuration.AppConfig;
+using System;
+
+namespace Microsoft.Extensions.Configuration
+{
+    /// <summary>
+    /// This class contains extension methods related to the <see cref="IConfigurationBuilder"/>
+    /// type.
+    /// </summary>
+    public static class ConfigurationBuilderExtensions
+    {
+        // *******************************************************************
+        // Public methods.
+        // *******************************************************************
+
+        #region Public methods
+
+        /// <summary>
+        /// This method injects an app.config source into the specified builder.
+        /// </summary>
+        /// <param name="builder">The builder to use for the operation.</param>
+        /// <param name="optional">True if the file is optional; false otherwise.</param>
+        /// <param name="reloadOnChange">True if the provider should reload the 
+        /// data from the underlying file changes; false otherwise.</param>
+        /// <returns>An <see cref="IConfigurationSource"/> object.</returns>
+        public static IConfigurationSource AddAppConfigSource(
+            this IConfigurationBuilder builder,
+            bool optional = true,
+            bool reloadOnChange = false
+            )
+        {
+            // Create the source.
+            var source = new AppConfigConfigurationSource(
+                optional, 
+                reloadOnChange
+                );
+
+            // Add the source to the builder.
+            builder.Add(source);
+
+            // Return the source.
+            return source;
+        }
+
+        #endregion
+    }
+}
---
+++ src/CG.Configuration/ServiceCollectionExtensions.cs
@@ -0,0 +1,99 @@
+﻿using CG.DataAnnotations;
+using CG.DataAnnotations.Options;
+using CG.Validations;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Microsoft.Extensions.Configuration
+{
+    /// <summary>
+    /// This class contains extension methods related to the <see cref="IServiceCollection"/>
+    /// type.
+    /// </summary>
+    public static partial class ServiceCollectionExtensions
+    {
+        // *******************************************************************
+        // Public methods.
+        // *******************************************************************
+
+        #region Public methods
+
+        /// <summary>
+        /// This method attempts to configure the specified options type with 
+        /// the service collection. It then returns a bound and validated options
+        /// instance.
+        /// </summary>
+        /// <typeparam name="TOptions">The type of associated options.</typeparam>
+        /// <param name="serviceCollection">The service collection to use for the 
+        /// operation.</param>
+        /// <param name="configuration">The configuration to use for the operation.</param>
+        /// <returns>The value of the <paramref name="serviceCollection"/>
+        /// parameter, for chaining calls together.</returns>
+        public static IServiceCollection TryConfigure<TOptions>(
+            this IServiceCollection serviceCollection,
+            IConfiguration configuration
+            ) where TOptions : class, new()
+        {
+            // Validate the parameters before attempting to use them.
+            Guard.Instance().ThrowIfNull(serviceCollection, nameof(serviceCollection))
+                .ThrowIfNull(configuration, nameof(configuration));
+
+            // Create the options.
+            var options = new TOptions();
+
+            // Bind the options to the configuration.
+            configuration.Bind(options);
+
+            // Verify the result - if possible.
+            (options as OptionsBase)?.ThrowIfInvalid();
+
+            // Add the options to the DI container.
+            serviceCollection.TryAddSingleton(options);
+
+            // Return the service collection.
+            return serviceCollection;
+        }
+
+        // *******************************************************************
+
+        /// <summary>
+        /// This method attempts to configure the specified options type with 
+        /// the service collection. It then returns a bound and validated options
+        /// instance.
+        /// </summary>
+        /// <typeparam name="TOptions">The type of associated options.</typeparam>
+        /// <param name="serviceCollection">The service collection to use for the 
+        /// operation.</param>
+        /// <param name="configuration">The configuration to use for the operation.</param>
+        /// <param name="options">The bound and validated options instance.</param>
+        /// <returns>The value of the <paramref name="serviceCollection"/>
+        /// parameter, for chaining calls together.</returns>
+        public static IServiceCollection TryConfigure<TOptions>(
+            this IServiceCollection serviceCollection,
+            IConfiguration configuration,
+            out TOptions options
+            ) where TOptions : class, new()
+        {
+            // Validate the parameters before attempting to use them.
+            Guard.Instance().ThrowIfNull(serviceCollection, nameof(serviceCollection))
+                .ThrowIfNull(configuration, nameof(configuration));
+
+            // Create the options.
+            options = new TOptions();
+
+            // Bind the options to the configuration.
+            configuration.Bind(options);
+
+            // Verify the result - if possible.
+            (options as OptionsBase)?.ThrowIfInvalid();
+
+            // Add the options to the DI container.
+            serviceCollection.TryAddSingleton(options);
+
+            // Return the service collection.
+            return serviceCollection;
+        }
+
+        #endregion
+    }
+}
